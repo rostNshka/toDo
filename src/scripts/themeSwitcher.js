2dd8deda550df3cac67e8d193e63c4b02d65c7ef
RostNshka <devkotder@ya.ru>
2025-10-29 16:21:41 +0400

    fix theme switch

--- src/scripts/themeSwitcher.js
+++ src/scripts/themeSwitcher.js
@@ -2,17 +2,42 @@ class ThemeSwitcher {
     constructor(buttonSelector) {
         this.button = document.querySelector('.theme__button');
         this.styleMode = localStorage.getItem("styleMode");
+
+        this.input = document.querySelectorAll('input');
+        this.svg = document.querySelectorAll("svg")
+        this.span = document.querySelectorAll("span");
+
         this.init();
     }
 
     enableDarkStyle() {
         document.body.classList.add('theme__dark');
+        this.input.forEach((item) => {
+            item.style.color = 'var(--light-color)';
+        })
+
+        this.svg.forEach((item) => {
+            item.style.color = 'var(--dark-color)';
+        })
+        this.span.forEach((item) => {
+            item.style.color = 'var(--light-color)';
+        })
+
         document.body.classList.remove('theme__light');
         localStorage.setItem("styleMode", 'dark');
     }
 
     disableDarkStyle() {
         document.body.classList.add('theme__light');
+        this.input.forEach((item) => {
+            item.style.color = 'var(--dark-color)';
+        })
+        this.svg.forEach((item) => {
+            item.style.color = 'var(--light-color)';
+        })
+        this.span.forEach((item) => {
+            item.style.color = 'var(--dark-color)';
+        })
         document.body.classList.remove('theme__dark');
         localStorage.setItem("styleMode", 'light');
     }
